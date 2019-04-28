54e38116e0a45b01d9dc6c39f976bcc5a25f87cd
Allen McDermott <alleng.mcd@gmail.com>
2019-04-27 20:04:44 -0500

    Doing some basic initialization

--- src/SingleServerQueueing.c
+++ src/SingleServerQueueing.c
@@ -1,4 +1,41 @@
+#define TRUE 1
+#define FALSE 0
+#define bool int
+#define status int
+#define inQueue 0
+#define inProcess 1
+#define finished 2
 
+struct queue {
+    struct customer *nextCustomer;
+    struct customer *currentCustomer;
+}  *firstCustomer, *lastCustomer;
+
+struct customer {
+    int id;
+    float enterQueue;
+    float leaveQueue;
+    float enterProcessing;
+    float leaveProcessing;
+    status customerStatus;
+};
+
+void processCustomer(struct customer *inCustomer)
+{
+
+}
+
+void enterQueue(struct customer *inCustomer)
+{
+    struct queue *tempQueue = malloc(sizeof(struct queue));
+    lastCustomer->nextCustomer = inCustomer;
+}
+
+void enterQueue(struct customer *inCustomer)
+{
+    struct queue *tempQueue = malloc(sizeof(struct queue));
+    lastCustomer->nextCustomer = inCustomer;
+}
 
 int main()
 {
