7492f98ce5542ab63c51b2651ae3cdcd9e205d76
Allen McDermott <alleng.mcd@gmail.com>
2019-05-05 10:04:16 -0500

    Finishing up the main functions

--- src/MaxSubArray.c
+++ src/MaxSubArray.c
@@ -7,7 +7,7 @@ struct FindMaxCrossingSubarrayReturn {
     int sum;
 };
 
-int FindMaxCrossingSubarray(int *A, int low, int mid, int high)
+struct FindMaxCrossingSubarrayReturn FindMaxCrossingSubarray(int *A, int low, int mid, int high)
 {
     int maxLeft;
     int leftSum = INT_MAX;
@@ -24,6 +24,7 @@ int FindMaxCrossingSubarray(int *A, int low, int mid, int high)
     }
 
     int rightSum = INT_MAX * -1;
+    int maxRight;
     sum = 0;
 
     for(int j = mid+1; j < high; j++)
@@ -32,16 +33,44 @@ int FindMaxCrossingSubarray(int *A, int low, int mid, int high)
         if(sum > rightSum)
         {
             rightSum = sum;
-            maxLeft = j;
+            maxRight = j;
         }
     }
 
-    return;
+    struct FindMaxCrossingSubarrayReturn returnValue = {maxLeft, maxRight, leftSum+rightSum};
+    return returnValue;
 }
 
-int FindMaxSubArray(int *inA, int low, int high)
+struct FindMaxCrossingSubarrayReturn FindMaxSubArray(int *A, int low, int high)
 {
-    
+    struct FindMaxCrossingSubarrayReturn right, left, cross;    
+
+    if(high == low)
+    {
+        struct FindMaxCrossingSubarrayReturn returnValue = { low, high, A[low] };
+        return returnValue;
+    }
+    else
+    {
+        int mid = (high + low) / 2;
+        right = FindMaxSubArray(A, low, mid);
+        left = FindMaxSubArray(A, mid + 1, high);
+        cross = FindMaxCrossingSubarray(A, low, mid, high);
+
+        if(left.sum >= right.sum && left.sum >= cross.sum)
+        {
+            return left;
+        }
+        else if(right.sum >= left.sum && right.sum >= cross.sum)
+        {
+            return right;
+        }
+        else
+        {
+            return cross;
+        }
+        
+    }
 }
 
 int main()
